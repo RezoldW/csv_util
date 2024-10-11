57bb176fbe789b987a3b391165a1c1dbb274dfb8
tonyplotnikov <145991108+RezoldW@users.noreply.github.com>
2024-10-11 17:19:05 +0500

    Test commit

--- main.go
+++ main.go
@@ -3,20 +3,10 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"github.com/sqweek/dialog"
 	"log"
-	"os"
 )
 
 func main() {
-	// Открываем диалоговое окно для выбора файла
-	filename, err := dialog.File().Filter("CSV Files", "csv").Title("Выберите CSV файл").Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Открываем выбранный файл
-	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,8 +19,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Выводим содержимое CSV
 	for _, record := range records {
 		fmt.Println(record)
 	}
 }
+
+// Test commit
